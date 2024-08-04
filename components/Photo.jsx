94748358d8dd9c6b4976f820f5f823e2c8da2981
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2024-08-04 18:25:35 +0530

    commiting

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -29,7 +29,7 @@ const Photo = () => {
         fill="transparent"
         viewBox="0 0 506 506"
         xmlns="http://www.w3.org/2000/svg">
-            <motion.circle cx="253" cy="253" r="250"  stroke='#00ff99' strokeWidth="6" strokeLinecap="round" strokeLinejoin="round"
+            <motion.circle cx="253" cy="253" r="250"  stroke='#ff8b00' strokeWidth="6" strokeLinecap="round" strokeLinejoin="round"
             initial={{strokeDasharray:"24 10 0 0"}}
             animate={{
                 strokeDasharray:["15 120 25 25","16 25 92 72","4 250 22 22"],
